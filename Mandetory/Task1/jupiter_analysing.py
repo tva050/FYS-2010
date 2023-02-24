b332d043ae85274a5252a47f2b3fa7011f1d47a2
tva050 <trym755@gmail.com>
2023-02-24 08:42:57 +0100

    dont

--- Mandetory/Task1/jupiter_analysing.py
+++ Mandetory/Task1/jupiter_analysing.py
@@ -4,7 +4,6 @@ import skimage
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
-import scipy as sc
 import scipy.io as sio
 
 plt.style.use('ggplot')
@@ -12,13 +11,14 @@ plt.style.use('ggplot')
 JUP1 = cv2.imread('Mandetory\Supplementary data\Jupiter1.png')
 JUP2 = cv2.imread('Mandetory\Supplementary data\Jupiter2.png')
 
-gray_JUP2 = cv2.imread('Mandetory\Supplementary data\Jupiter2.png', 0) # Jupiter2 in grayscale
-blue, green, red = cv2.split(JUP1)  # Jupiter1 in RGB
+gray_JUP2 = cv2.imread('Mandetory\Supplementary data\Jupiter2.png',0)
+blue, green, red = cv2.split(JUP1)
 
 
 """ Task 1b """
+
 def histogram(img):
-    for i, col in enumerate(['b', 'g', 'r']):
+    for i, col in enumerate(['r', 'g', 'b']):
         hist = cv2.calcHist([img], [i], None, [256], [0, 256])
         plt.plot(hist, color = col)
         plt.title("Histogram of Jupiter1")
@@ -33,7 +33,7 @@ def histogram(img):
 def magnitude_spectrum(img): 
     f = np.fft.fft2(img)
     fshift = np.fft.fftshift(f)
-    magnitude_spectrum = 20*np.log(np.abs(fshift))
+    magnitude_spectrum = np.log(np.abs(fshift))
     plt.subplot(121),plt.imshow(img, cmap = 'gray')
     plt.xticks([])
     plt.yticks([])
@@ -43,67 +43,8 @@ def magnitude_spectrum(img):
     plt.xticks([])
     plt.yticks([])
     plt.show()
-    
-def restore_jupiter1():
-    # Starting by filter out the salt-and-pepper noise using madian filter
-    JUp1 = np.array(Image.open('Mandetory\Supplementary data\Jupiter1.png'))
-    jup1_medfiltered = sc.ndimage.median_filter(JUp1, size=3)
-    plt.imshow(jup1_medfiltered)
-    plt.xticks([])
-    plt.yticks([])
-    plt.title("Median filter")
-    plt.show()
-    
-    def notch_filter(shape, d0=9, u_k=0, v_k=0):
-        P, Q = shape
-        # initialize the filter with zeros
-        H = np.zeros((P, Q))
 
-        # Travers through the filter and calculate the value of each pixel
-        for u in range(0, P):
-            for v in range(0, Q):
-                D_k = np.sqrt((u - P/2 + u_k)**2 + (v - Q/2 + v_k)**2)
-                D_mk = np.sqrt((u - P/2 - u_k)**2 + (v - Q/2 - v_k)**2)
-                if D_k <= d0 or D_mk <= d0:
-                    H[u, v] = 0.0
-                else:
-                    H[u, v] = 1.0
-        return H
+#histogram(JUP1)
 
-    #gray_JUP1 = cv2.imread('Mandetory\Supplementary data\Jupiter1.png',0)
-    gray_JUP1 = red
-
-    f = np.fft.fft2(gray_JUP1)
-    fshift = np.fft.fftshift(f)
-    phase_spectrumR = np.angle(fshift)
-    magnitude_spectrum = 20*np.log(np.abs(fshift))
-
-    img_shape =  red.shape 
-
-    H1 = notch_filter(img_shape, 4, 5, 10)
-
-    NotchFilter = H1 
-    NotchRejectCenter = fshift * NotchFilter 
-    NotchReject = np.fft.ifftshift(NotchRejectCenter)
-    inverse_NotchReject = np.fft.ifft2(NotchReject)  # Compute the inverse DFT of the result
-
-    Result = np.abs(inverse_NotchReject)
-
-    plt.imshow(Result, "gray")
-    plt.xticks([])
-    plt.yticks([])
-    plt.title("Notch filter")
-    plt.show()        
-
-    plt.imshow(magnitude_spectrum*NotchFilter, "gray") 
-    plt.title("Notch Reject Filter")
-    plt.xticks([])
-    plt.yticks([])
-    plt.show()
-#histogram(JUP1) # Jupiter1 in RGB
-#magnitude_spectrum(red) # red channel of Jupiter1
-#magnitude_spectrum(gray_JUP2) # Jupiter2 in grayscale
-restore_jupiter1()
-    
-
-
+magnitude_spectrum(red)
+#magnitude_spectrum(gray_JUP2)
---
+++ Mandetory/Task1/restoring_jupiter1.py
@@ -0,0 +1,18 @@
+from PIL import Image
+import cv2
+import skimage
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import scipy.io as sio
+
+plt.style.use('ggplot')
+
+
+
+
+
+
+
+
+
