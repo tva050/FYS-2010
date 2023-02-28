7dc6f2d20e38bcca04c742a27999dd89a326a0dd
tva050 <trym755@gmail.com>
2023-02-28 10:04:44 +0100

    Median, noch and CHM filter

--- Mandetory/Task1/jupiter_analysing.py
+++ Mandetory/Task1/jupiter_analysing.py
@@ -15,8 +15,7 @@ gray_JUP2 = cv2.imread('Mandetory\Supplementary data\Jupiter2.png',0)
 blue, green, red = cv2.split(JUP1)
 
 
-""" Task 1b """
-
+""" --------------- Task 1b ---------------"""
 def histogram(img):
     for i, col in enumerate(['b', 'g', 'r']):
         hist = cv2.calcHist([img], [i], None, [256], [0, 256])
@@ -44,13 +43,16 @@ def magnitude_spectrum(img):
     plt.yticks([])
     plt.show()
 
+    
 #histogram(JUP1)
-magnitude_spectrum(red)
+#magnitude_spectrum(red)
 #magnitude_spectrum(gray_JUP2)
 
-""" Task 1c """
-orginal = cv2.merge((blue, green, red))
 
+""" --------------- Task 1c ---------------"""
+
+orginal = cv2.merge((blue, green, red))
+# __Notch filter__
 # Source: https://stackoverflow.com/questions/65483030/notch-reject-filtering-in-python
 def notch_filter(shape, d0, u_k, v_k): 
     M, N = shape
@@ -64,13 +66,12 @@ def notch_filter(shape, d0, u_k, v_k):
             vertical1 =v - N/2 + v_k
             horizontal2 = u - M/2 - u_k 
             vertical2 = v - N/2 - v_k
-           
+        
             if D_k <= d0 or D_mk <= d0 or horizontal1 == 0 or vertical1 == 0 or horizontal2 == 0 or vertical2 == 0:
                 H[u, v] = 0.0
             else: 
                 H[u, v] = 1.0
     return H
-
 f = np.fft.fft2(red) 
 fshift = np.fft.fftshift(f)
 magnitude_spectrum = np.log(np.abs(fshift))
@@ -81,88 +82,83 @@ red = fshift * H1
 red = np.fft.ifftshift(red)
 red = np.fft.ifft2(red)
 red = np.abs(red)
-
 red = np.uint8(red)
-
-plt.imshow(magnitude_spectrum*H1, cmap = 'gray')
-plt.xticks([])
-plt.yticks([])
-plt.show()
 
 notch_filtered = cv2.merge((blue, green, red))
 
-blue = ndi.median_filter(blue, size = 3)
-#green = ndi.median_filter(green, size = 5)
+# __Median filter__
+median_filter = ndi.median_filter(cv2.merge((blue, green, red)), size=3)
 
-""" def CHM_operator(roi,q):
-    roi = roi.astype(np.float64) #Copy of the roi
-    dividend= np.mean((roi)**(q+1)) # 
-    divisor=np.mean((roi)**(q))
-    quo= dividend/divisor
-    return quo
-def CHM_Algo(image,q):
-    new_image = np.zeros(image.shape)
-    image = cv2.copyMakeBorder(image,1,1,1,1,cv2.BORDER_DEFAULT)
-    for i in range(1,image.shape[0]-1):
-        for j in range(1,image.shape[1]-1):
-            a=i-1
-            b=j-1
-            new_image[a,b] = CHM_operator(image[a:a+3,b:b+3],q)
+# __Contrast Harmonic Mean filter__
+np.seterr(invalid="ignore") # To ignore the warning of division by zero
+# Contrast Harmonic Mean filter, 
+# written with help from https://stackabuse.com/introduction-to-image-processing-in-python-with-opencv/
+def CHM_filter(img, Q):
+    img = img.astype(np.float64)
+    numirator = img**(Q+1)
+    denumirator = (img)**(Q)
+    kernel = np.full(shape = 3, fill_value= 1.0, dtype = np.float64)
+    result = cv2.filter2D(numirator, -1, kernel) / cv2.filter2D(denumirator, -1, kernel)
+    return result
+
+CHM_filtered = CHM_filter(median_filter, -2.)
+CHM_filtered = np.uint8(CHM_filtered)
+
+# __Plotting__
+def plot_notch_filtered_J1img_mag():
+    plt.rcParams["figure.figsize"] = [7.00, 3.50]
+    plt.rcParams["figure.autolayout"] = True
+    plt.rcParams["axes.grid"] = False
+    plt.subplot(1, 3, 1)
+    plt.imshow(cv2.cvtColor(orginal, cv2.COLOR_BGR2RGB))
+    plt.xticks([])
+    plt.yticks([])
+    plt.title("Orginal Jupiter1")
+    plt.subplot(1, 3, 2)
+    plt.imshow(magnitude_spectrum*H1, cmap = 'gray')
+    plt.xticks([])
+    plt.yticks([])
+    plt.title("Eliminated bursts")
+    plt.subplot(1, 3, 3)
+    plt.imshow(cv2.cvtColor(notch_filtered, cv2.COLOR_BGR2RGB))
+    plt.xticks([])
+    plt.yticks([])
+    plt.title("Notch filtered Jupiter1")
+    plt.show()
+
+def plot_median_CHM_filtered_J1img():
+    plt.rcParams["figure.figsize"] = [7.00, 3.50]
+    plt.rcParams["figure.autolayout"] = True
+    plt.rcParams["axes.grid"] = False
+    plt.subplot(1, 3, 1)
+    plt.imshow(cv2.cvtColor(notch_filtered, cv2.COLOR_BGR2RGB))
+    plt.xticks([])
+    plt.yticks([])
+    plt.title("Notch filtered Jupiter1")
+    plt.subplot(1, 3, 2)
+    plt.imshow(cv2.cvtColor(median_filter, cv2.COLOR_BGR2RGB))
+    plt.xticks([])
+    plt.yticks([])
+    plt.title("Median filtered")
+    plt.subplot(1, 3, 3)
+    plt.imshow(cv2.cvtColor(CHM_filtered, cv2.COLOR_BGR2RGB))
+    plt.xticks([])
+    plt.yticks([])
+    plt.title("CHM filtered")
+    plt.show()
     
-    i_min=np.min(image) # min pixel
-    i_max=np.max(image) # max pixel
-    new_image = (new_image-i_min) * (255/i_max)
-    new_image = new_image.astype(np.uint8)
-    return new_image """
-
-def CHM_filter(img, q):
+def restored_JUP1_image():
+    plt.imshow(cv2.cvtColor(CHM_filtered, cv2.COLOR_BGR2RGB))
+    plt.xticks([])
+    plt.yticks([])
+    plt.title("Restored Jupiter1")
+    plt.show()
     
-
-median_filter = cv2.merge((blue, green, red))
-
-plt.rcParams["figure.figsize"] = [7.00, 3.50]
-plt.rcParams["figure.autolayout"] = True
-plt.rcParams["axes.grid"] = False
-plt.subplot(1, 3, 1)
-plt.imshow(cv2.cvtColor(orginal, cv2.COLOR_BGR2RGB))
-plt.xticks([])
-plt.yticks([])
-plt.title("Orginal Jupiter1")
-plt.subplot(1, 3, 2)
-plt.imshow(magnitude_spectrum*H1, cmap = 'gray')
-plt.xticks([])
-plt.yticks([])
-plt.title("Eliminated bursts")
-plt.subplot(1, 3, 3)
-plt.imshow(cv2.cvtColor(notch_filtered, cv2.COLOR_BGR2RGB))
-plt.xticks([])
-plt.yticks([])
-plt.title("Notch filtered Jupiter1")
-plt.show()
-
-plt.subplot(121)
-plt.imshow(cv2.cvtColor(orginal, cv2.COLOR_BGR2RGB))
-plt.xticks([])
-plt.yticks([])
-plt.title("Orginal Jupiter1")
-plt.subplot(122)
-plt.imshow(cv2.cvtColor(notch_filtered, cv2.COLOR_BGR2RGB))
-plt.xticks([])
-plt.yticks([])
-plt.title("Notch filtered Jupiter1")
+# plot_notch_filtered_J1img_mag()
+# plot_median_CHM_filtered_J1img()
+restored_JUP1_image()
 
 
-plt.subplot(121)
-plt.imshow(cv2.cvtColor(notch_filtered, cv2.COLOR_BGR2RGB))
-plt.xticks([])
-plt.yticks([])
-plt.title("Notch filtered Jupiter1")
-plt.subplot(122)
-plt.imshow(cv2.cvtColor(median_filter, cv2.COLOR_BGR2RGB))
-plt.xticks([])
-plt.yticks([])
-plt.title("Median filtered Jupiter1")
-plt.show()
-
+""" --------------- Task 1d ---------------"""
 
 cv2.waitKey(0)  
