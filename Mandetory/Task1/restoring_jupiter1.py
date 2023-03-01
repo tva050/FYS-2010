729cdab4540213f99cd4ab9b15e0c13499a1ad7a
tva050 <trym755@gmail.com>
2023-03-01 14:15:56 +0100

    LEtts

--- Mandetory/Task1/restoring_jupiter1.py
+++ Mandetory/Task1/restoring_jupiter1.py
@@ -11,7 +11,7 @@ JUP1 = cv2.imread('Mandetory\Supplementary data\Jupiter1.png')
 
 blue, green, red = cv2.split(JUP1)
 
-orginal = cv2.merge((blue, green, red))
+orginal_jup1 = cv2.merge((blue, green, red))
 
 # __Notch filter__
 # Source: https://stackoverflow.com/questions/65483030/notch-reject-filtering-in-python
@@ -67,9 +67,65 @@ CHM_filtered_j1 = CHM_filter(median_filter, -2.)
 CHM_filtered_j1 = np.uint8(CHM_filtered_j1)
 
 
+def plot_notch_filtered_J1img_mag():
+    plt.rcParams["figure.figsize"] = [7.00, 3.50]
+    plt.rcParams["figure.autolayout"] = True
+    plt.rcParams["axes.grid"] = False
+    plt.subplot(1, 3, 1)
+    plt.imshow(cv2.cvtColor(orginal_jup1, cv2.COLOR_BGR2RGB))
+    plt.xticks([])
+    plt.yticks([])
+    plt.title("Orginal Jupiter1")
+    plt.subplot(1, 3, 2)
+    plt.imshow(magnitude_spectrum_J1*H1_J1, cmap = 'gray')
+    plt.xticks([])
+    plt.yticks([])
+    plt.title("Eliminated bursts")
+    plt.subplot(1, 3, 3)
+    plt.imshow(cv2.cvtColor(notch_filtered, cv2.COLOR_BGR2RGB))
+    plt.xticks([])
+    plt.yticks([])
+    plt.title("Notch filtered Jupiter1")
+    plt.show()
 
-
-
+def plot_median_CHM_filtered_J1img():
+    plt.rcParams["figure.figsize"] = [7.00, 3.50]
+    plt.rcParams["figure.autolayout"] = True
+    plt.rcParams["axes.grid"] = False
+    plt.subplot(1, 3, 1)
+    plt.imshow(cv2.cvtColor(notch_filtered, cv2.COLOR_BGR2RGB))
+    plt.xticks([])
+    plt.yticks([])
+    plt.title("Notch filtered Jupiter1")
+    plt.subplot(1, 3, 2)
+    plt.imshow(cv2.cvtColor(median_filter, cv2.COLOR_BGR2RGB))
+    plt.xticks([])
+    plt.yticks([])
+    plt.title("Median filtered")
+    plt.subplot(1, 3, 3)
+    plt.imshow(cv2.cvtColor(CHM_filtered_j1, cv2.COLOR_BGR2RGB))
+    plt.xticks([])
+    plt.yticks([])
+    plt.title("CHM filtered")
+    plt.show()
+    
+def restored_JUP1_image():
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.imshow(cv2.cvtColor(orginal_jup1, cv2.COLOR_BGR2RGB))
+    ax1.set_title("Orginal Jupiter1")
+    ax2.imshow(cv2.cvtColor(CHM_filtered_j1, cv2.COLOR_BGR2RGB))
+    ax2.set_title("Restored Jupiter1")
+    fig.suptitle("$\Longrightarrow$\n$\Longrightarrow$", x = 0.52, y = 0.45, fontsize = 16)
+    fig.tight_layout(pad = 3.5)
+    
+    # remove the x and y ticks
+    for ax in fig.get_axes():
+        ax.set_xticks([])
+        ax.set_yticks([])
+    
+    plt.show()
+    
+    
 
 
 
