9a40cb2422d783a03b60b14369b3ecf909454ca0
tva050 <trym755@gmail.com>
2023-03-05 11:39:52 +0100

    Finish task1

--- Mandetory/Task1/restoring_jupiter1.py
+++ Mandetory/Task1/restoring_jupiter1.py
@@ -7,68 +7,120 @@ import matplotlib.colors as colors
 import scipy.io as sio
 import scipy.ndimage as ndi
 from PIL import Image
+from scipy.signal import wiener
 
-JUP1 = cv2.imread('Mandetory\Supplementary data\Jupiter1.png') 
+JUP2 = cv2.imread('Mandetory\Supplementary data\Jupiter2.png') 
 
-blue, green, red = cv2.split(JUP1)
+blue_j2, green_j2, red_j2 = cv2.split(JUP2)
 
-orginal_jup1 = cv2.merge((blue, green, red))
-
-# __Notch filter__
-# Source: https://stackoverflow.com/questions/65483030/notch-reject-filtering-in-python
-def notch_filter_J1(shape, d0, u_k, v_k): 
-    M, N = shape
-    H = np.zeros((M, N))
-    
-    for u in range(0, M):
-        for v in range(0, N):
-            D_k = np.sqrt((u - M/2 + u_k)**2 + (v - N/2 + v_k)**2)
-            D_mk = np.sqrt((u - M/2 - u_k)**2 + (v - N/2 - v_k)**2)
-            horizontal1 = u - M/2 + u_k 
-            vertical1 =v - N/2 + v_k
-            horizontal2 = u - M/2 - u_k 
-            vertical2 = v - N/2 - v_k
-        
-            if D_k <= d0 or D_mk <= d0 or horizontal1 == 0 or vertical1 == 0 or horizontal2 == 0 or vertical2 == 0:
-                H[u, v] = 0.0
-            else: 
-                H[u, v] = 1.0
-    return H
-
-f_J1 = np.fft.fft2(red) 
-fshift_J1 = np.fft.fftshift(f_J1)
-magnitude_spectrum_J1 = np.log(np.abs(fshift_J1))
-
-H1_J1 = notch_filter_J1(red.shape, 3, 7, 7)
-
-red = fshift_J1 * H1_J1
-red = np.fft.ifftshift(red)
-red = np.fft.ifft2(red)
-red = np.abs(red)
-red = np.uint8(red)
-
-notch_filtered = cv2.merge((blue, green, red))
-
-
-# __Median filter__
-median_filter = ndi.median_filter(notch_filtered, size=3)
-
-
-# __Contrast Harmonic Mean filter__
-np.seterr(invalid="ignore") # 
-
+np.seterr(invalid="ignore") # To ignore the warning of division by zero
+# Contrast Harmonic Mean filter, 
+# written with help from https://stackabuse.com/introduction-to-image-processing-in-python-with-opencv/
 def CHM_filter(img, Q):
-    img = img.astype(np.float64)
+    img = img.astype(np.float64) 
     numirator = img**(Q+1)
     denumirator = img**(Q)
     kernel = np.full(shape = 3, fill_value= 1.0, dtype = np.float64)
     result = cv2.filter2D(numirator, -1, kernel) / cv2.filter2D(denumirator, -1, kernel)
     return result
+median_filter_j2 = cv2.medianBlur(JUP2, 3)
 
-CHM_filtered_j1 = CHM_filter(median_filter, -2.)
-CHM_filtered_j1 = np.uint8(CHM_filtered_j1)
+CHM_filtered_j2 = CHM_filter(median_filter_j2, 3)
+CHM_filtered_j2 = np.uint8(CHM_filtered_j2)
+blue_, green_, red_ = cv2.split(CHM_filtered_j2)
+def wiener_filter(img):
+    img = img.astype(np.float64)
+    img = np.array(img)
+    img = wiener(img, (5, 5))
+    img = np.uint8(img)
+    return img
+blue_ = wiener_filter(blue_)
+green_ = wiener_filter(green_)
+red_ = wiener_filter(red_)
 
-blue_, green_, red_ = cv2.split(CHM_filtered_j1)
+wiener_filtered  = cv2.merge((blue_, green_, red_))
+
+blue, green, red = cv2.split(wiener_filtered)
+
+def notch_filter_J2(shape, d0, u_k, v_k): 
+    M, N = shape
+    H = np.zeros((M, N))
+    for u in range(0, M):
+        for v in range(0, N):
+            D_k = np.sqrt((u - M/2 + u_k)**2 + (v - N/2 + v_k)**2)
+            D_mk = np.sqrt((u - M/2 - u_k)**2 + (v - N/2 - v_k)**2)
+            if D_k <= d0 or D_mk <= d0 :
+                H[u, v] = 0.0
+            else: 
+                H[u, v] = 1.0
+    return H
+
+def fourier_transform_J2(img):
+    f = np.fft.fft2(img)
+    fshift = np.fft.fftshift(f)
+    magnitude_spectrum = np.log(np.abs(fshift))
+    
+    H1 = notch_filter_J2(img.shape, 2, 5, 0)
+
+    img = fshift * H1
+    img = np.fft.ifftshift(img)
+    img = np.fft.ifft2(img)
+    img = np.abs(img)
+    img = np.uint8(img)
+    
+    return img
+
+blue = fourier_transform_J2(blue)
+green = fourier_transform_J2(green)
+red = fourier_transform_J2(red)
+#-------------
+f = np.fft.fft2(blue)
+fshift = np.fft.fftshift(f)
+magnitude_spectrum = np.log(np.abs(fshift))
+H1 = notch_filter_J2(blue.shape, 2, 5, 0)
+
+#-------------
+
+notch_filtered_j2 = cv2.merge((blue, green, red))
+n_bluej2, n_greenj2, n_redj2 = cv2.split(notch_filtered_j2)
+def homomorphic_filter(img, gL, gH, c, D0):
+    img = img.astype(np.float64)
+    img = np.log1p(img) # log transform to reduce the effect of dark pixels
+
+    M, N = img.shape
+    u, v = np.meshgrid(np.arange(0, N), np.arange(0, M))
+    Duv = np.sqrt((u - N/2)**2 + (v - M/2)**2)
+    highpass = (gH - gL) * (1 - np.exp(-(c * Duv**2) / (D0**2))) + gL       # GHPF (Gaussian High Pass Filter)
+    #highpass = (1.0 - 1.0 / (1.0 + (D0 / Duv)**(2 * c))) * (gH - gL) + gL  # BHPF (Butterworth High Pass Filter)
+    
+    fft_img = np.fft.fftshift(np.fft.fft2(img)) # fft  
+    filtering_img = fft_img * highpass # filtering image
+    filtered_img_log = np.fft.ifft2(np.fft.ifftshift(filtering_img)) # ifft shift
+    
+    # convert back to the original scale 
+    img_filtered = np.exp(filtered_img_log) - 1
+    img_filtered = np.real(img_filtered)
+    img_filtered = np.uint8(img_filtered * 255 / np.max(img_filtered)) 
+    
+    return img_filtered
+
+n_bluej2  = homomorphic_filter(n_bluej2,  2.5, 0.7, 20, 300)
+n_greenj2 = homomorphic_filter(n_greenj2, 2.5, 0.7, 20, 300)
+n_redj2   = homomorphic_filter(n_redj2,   2.5, 0.7, 20, 300)
+
+homomorphic_filtered_j2 = cv2.merge((n_bluej2, n_greenj2, n_redj2))
+h_bluej2, h_greenj2, h_redj2 = cv2.split(homomorphic_filtered_j2)
+
+plt.subplot(1, 2, 1)
+plt.imshow(cv2.cvtColor(notch_filtered_j2, cv2.COLOR_BGR2RGB))
+plt.xticks([]), plt.yticks([])
+plt.title("Notch filtered")
+plt.subplot(1, 2, 2)
+plt.imshow(cv2.cvtColor(homomorphic_filtered_j2, cv2.COLOR_BGR2RGB))
+plt.xticks([]), plt.yticks([])
+plt.title("Homomorphic filtered")
+plt.show()
+
 
 def contrast_stretching(img):
     img = img.astype(np.float64)
@@ -77,105 +129,71 @@ def contrast_stretching(img):
     img = img * 255
     return img
 
-blue_ = contrast_stretching(blue_)
-green_ = contrast_stretching(green_)
-red_ = contrast_stretching(red_)
+contrast_stretching_j2 = cv2.merge((contrast_stretching(h_bluej2), contrast_stretching(h_greenj2), contrast_stretching(h_redj2)))
+contrast_stretching_j2 = np.uint8(contrast_stretching_j2)
+plt.imshow(cv2.cvtColor(contrast_stretching_j2, cv2.COLOR_BGR2RGB))
+plt.xticks([]), plt.yticks([])
+plt.title("Contrast stretching")
+plt.show()
 
-contrast_stretching = cv2.merge((blue_, green_, red_))
-contrast_stretching = np.uint8(contrast_stretching)
-_blue_, _green_, _red_ = cv2.split(contrast_stretching)
+def wiener_filter(img):
+    img = img.astype(np.float64)
+    img = np.array(img)
+    img = wiener(img, (5, 5))
+    img = np.uint8(img)
+    return img
+
+blue_win = wiener_filter(h_bluej2)
+green_win = wiener_filter(h_greenj2)
+red_win = wiener_filter(h_redj2)
+wiener_filtered_j2 = cv2.merge((blue_win, green_win, red_win))
+
+plt.subplot(1, 2, 1)
+plt.imshow(cv2.cvtColor(contrast_stretching_j2, cv2.COLOR_BGR2RGB))
+plt.xticks([]), plt.yticks([])
+plt.title("Contrast stretching")
+plt.subplot(1, 2, 2)
+plt.imshow(cv2.cvtColor(wiener_filtered_j2, cv2.COLOR_BGR2RGB))
+plt.xticks([]), plt.yticks([])
+plt.title("Wiener filtered")
+plt.show()
 
 
-""" def laplacian_filter(img):
+    
+
+def laplacian_filter(img):
     kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
     img = cv2.filter2D(img, -1, kernel)
     return img
 
-laplacian_filter = laplacian_filter(contrast_stretching) """
+""" laplacian_filtered_j2 = laplacian_filter(contrast_stretching_j2)
+laplacian_filtered_j2 = np.uint8(laplacian_filtered_j2)
+
+plt.subplot(1, 2, 1)
+plt.imshow(cv2.cvtColor(contrast_stretching_j2, cv2.COLOR_BGR2RGB))
+plt.xticks([]), plt.yticks([])
+plt.title("Contrast stretching")
+plt.subplot(1, 2, 2)
+plt.imshow(cv2.cvtColor(laplacian_filtered_j2, cv2.COLOR_BGR2RGB))
+plt.xticks([]), plt.yticks([])
+plt.title("Laplacian filtered")
+plt.show() """
+
+""" gaussain = cv2.GaussianBlur(contrast_stretching_j2, (3, 3), 0)
+
+plt.imshow(cv2.cvtColor(gaussain, cv2.COLOR_BGR2RGB))
+plt.xticks([]), plt.yticks([])
+plt.title("Gaussian")
+plt.show() """
 
 
+""" 
+new_contra_harmonic_mean = CHM_filter(contrast_stretching_j2, 3)
+new_contra_harmonic_mean = np.uint8(new_contra_harmonic_mean)
 
-def plot_J1img_mag():
-    plt.imshow(magnitude_spectrum_J1*H1_J1, cmap = 'gray')
-    plt.xticks([])
-    plt.yticks([])
-    plt.title("Eliminated bursts")
-    plt.show()
-
-def plot_notch_filtered_J1img_mag():
-    plt.rcParams["figure.figsize"] = [7.00, 3.50]
-    plt.rcParams["figure.autolayout"] = True
-    plt.rcParams["axes.grid"] = False
-    plt.subplot(1, 3, 1)
-    plt.imshow(cv2.cvtColor(orginal_jup1, cv2.COLOR_BGR2RGB))
-    plt.xticks([])
-    plt.yticks([])
-    plt.title("Orginal Jupiter1")
-    plt.subplot(1, 3, 2)
-    plt.imshow(magnitude_spectrum_J1*H1_J1, cmap = 'gray')
-    plt.xticks([])
-    plt.yticks([])
-    plt.title("Eliminated bursts")
-    plt.subplot(1, 3, 3)
-    plt.imshow(cv2.cvtColor(notch_filtered, cv2.COLOR_BGR2RGB))
-    plt.xticks([])
-    plt.yticks([])
-    plt.title("Notch filtered Jupiter1")
-    plt.show()
-
-def plot_median_CHM_filtered_J1img():
-    plt.rcParams["figure.figsize"] = [7.00, 3.50]
-    plt.rcParams["figure.autolayout"] = True
-    plt.rcParams["axes.grid"] = False
-    plt.subplot(1, 3, 1)
-    plt.imshow(cv2.cvtColor(notch_filtered, cv2.COLOR_BGR2RGB))
-    plt.xticks([])
-    plt.yticks([])
-    plt.title("Notch filtered Jupiter1")
-    plt.subplot(1, 3, 2)
-    plt.imshow(cv2.cvtColor(median_filter, cv2.COLOR_BGR2RGB))
-    plt.xticks([])
-    plt.yticks([])
-    plt.title("Median filtered")
-    plt.subplot(1, 3, 3)
-    plt.imshow(cv2.cvtColor(CHM_filtered_j1, cv2.COLOR_BGR2RGB))
-    plt.xticks([])
-    plt.yticks([])
-    plt.title("CHM filtered")
-    plt.show()
-    
-def restored_JUP1_image():
-    fig, (ax1, ax2) = plt.subplots(1, 2)
-    ax1.imshow(cv2.cvtColor(orginal_jup1, cv2.COLOR_BGR2RGB))
-    ax1.set_title("Orginal Jupiter1")
-    ax2.imshow(cv2.cvtColor(CHM_filtered_j1, cv2.COLOR_BGR2RGB))
-    ax2.set_title("Restored Jupiter1")
-    fig.suptitle("$\Longrightarrow$\n$\Longrightarrow$", x = 0.52, y = 0.45, fontsize = 16)
-    fig.tight_layout(pad = 3.5)
-    
-    # remove the x and y ticks
-    for ax in fig.get_axes():
-        ax.set_xticks([])
-        ax.set_yticks([])
-    
-    plt.show()
-    
-def plot_J1img_contrast():
-    plt.subplot(121)
-    plt.imshow(cv2.cvtColor(CHM_filtered_j1, cv2.COLOR_BGR2RGB))
-    plt.xticks([]), plt.yticks([])
-    plt.title('Orginal Jupiter1')
-    plt.subplot(122)
-    plt.imshow(cv2.cvtColor(contrast_stretching, cv2.COLOR_BGR2RGB))
-    plt.xticks([]), plt.yticks([])
-    plt.title('Contrast stretching')
-    plt.show()
-
-
-#___________RUN FUNCTIONS___________
-#plot_J1img_mag()
-#plot_notch_filtered_J1img_mag()
-#plot_median_CHM_filtered_J1img()
-#restored_JUP1_image()
-plot_J1img_contrast()
-#___________________________________
+plt.imshow(cv2.cvtColor(new_contra_harmonic_mean, cv2.COLOR_BGR2RGB))
+plt.xticks([]), plt.yticks([])
+plt.title("New Contra Harmonic Mean")
+plt.show()
+"""
+ 
