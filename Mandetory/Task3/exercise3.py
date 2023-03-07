0805ca57a5ad0289657de95369940873ebb7e1b0
tva050 <trym755@gmail.com>
2023-03-07 13:10:15 +0100

    exc3

--- Mandetory/Task3/exercise3.py
+++ Mandetory/Task3/exercise3.py
@@ -1,6 +1,24 @@
 import numpy as np
 import scipy as sc 
+from scipy import signal
 
-w = ([[0,0,1], [0,1,2], [1,2,3]])
-f = ([[0,1,0], [0,1,0], [1,0,0]])
+w = ([[0,0,1], 
+      [0,1,2], 
+      [1,2,3]])
 
+f = ([[0,1,0], 
+      [0,1,0], 
+      [1,0,0]])
+
+
+grad = sc.signal.convolve2d(w, f, mode='same', boundary='wrap', fillvalue=0)
+print(grad)
+
+w_zeropadded = np.pad(w, 1, 'constant', constant_values=0)
+f_zeropadded = np.pad(f, 1, 'constant', constant_values=0)
+
+gra_1 = sc.signal.convolve2d(w_zeropadded, f_zeropadded, mode='same', boundary='wrap', fillvalue=0)
+print(gra_1)
+
+
+# cyclic convolution
